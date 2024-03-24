b5647c2e09bcc46cd6dfed3538cee26f53455b4e
Orin10120 <rindang3001@gmail.com>
2024-03-24 09:51:24 +0700

    Menambahkan class SayaTubeVideo

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpModul6_1302223023
+{
+    
+     public class Program
+    {
+        static void Main(string[] args)
+        {
+            String title = "Tutorial Design By Contract - Rindang Bani Isyan";
+            SayaTubeVideo vid = new SayaTubeVideo(title);
+            vid.IncreasePlayCount(3);
+            vid.printVideoDetails();
+        }
+    }
+}
---
+++ SayaTubeVideo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpModul6_1302223023
+{
+    public class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+       public SayaTubeVideo(String title)
+        {
+            this.title = title;
+            this.id = randomID();
+            this.playCount = 0;
+        }
+
+        private int randomID() { 
+            Random acak = new Random();
+            return 10000 + acak.Next(90000);
+        }
+
+        public void IncreasePlayCount(int playCount)
+        {
+            this.playCount = playCount;
+            this.playCount++;
+        }
+
+        public void printVideoDetails()
+        {
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Total PlayCount: " + playCount);
+        }
+    }
+}
